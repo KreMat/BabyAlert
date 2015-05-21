2e334fce6b20093de0912a4831990d52c81b834e
Matthias Kreuzriegler <matthias.kreuzriegler@gmx.at>
2015-05-22 00:58:54 +0200

    BabyAlert

---
+++ app/src/main/java/at/kreuzriegler/babyalert/AlarmRestClient.java
@@ -0,0 +1,28 @@
+package at.kreuzriegler.babyalert;
+
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.SyncHttpClient;
+
+/**
+ * Created by Matthias on 21.05.2015.
+ */
+public class AlarmRestClient {
+
+    private static final String BASE_URL = "http://alarm.kreuzriegler.at/";
+
+    private static SyncHttpClient client = new SyncHttpClient();
+
+    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return BASE_URL + relativeUrl;
+    }
+
+}
